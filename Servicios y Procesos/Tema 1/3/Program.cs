c83974437b5da63416618d2def011fd38a1d669b
Soul <51430864+Pazsaa@users.noreply.github.com>
2019-11-11 20:09:30 +0100

    added serv 3

---
+++ Servicios y Procesos/Tema 1/3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Serv3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            new Thread(() =>
+            {
+                while (x < 1000 && x > -1000)
+                {
+                    x++;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(x);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                while (x < 1000 && x > -1000)
+                {
+                    x--;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(x);
+                }
+            }).Start();
+        }
+    }
+}
