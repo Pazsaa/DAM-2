0a5974deb71ad828ee99d99c02a8f7f84eca6e2e
Pazsaa <apazgarcia18@outlook.es>
2020-02-04 13:31:39 +0100

    added 2 missing commands

---
+++ Servicios y Procesos/Tema 3/Networking2/Cliente/Cliente/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    class Program
+    {
+        private static readonly object _lock = new object();
+
+        static void Main(string[] args)
+        {
+            IPEndPoint ie = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 31416);
+
+            using (Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                try
+                {
+                    server.Connect(ie);
+                }
+                catch(SocketException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+
+                Console.WriteLine("Introduce tu nombre de usuario: ");
+                // Server reads username here.
+
+                Thread read = new Thread(o => ReadThread(server));
+                read.Start();
+
+                while (true)
+                {
+                    using (NetworkStream ns = new NetworkStream(server))
+                    using (StreamWriter sw = new StreamWriter(ns))
+                    {
+                        string message = Console.ReadLine();
+                        sw.WriteLine(message);
+                    }
+                }
+            }
+        }
+
+        public static void WriteThread(Socket server)
+        {
+            while (true)
+            {
+                using(NetworkStream ns = new NetworkStream(server))
+                using (StreamWriter sw = new StreamWriter(ns))
+                {
+                    string message = Console.ReadLine();
+                    sw.WriteLine(message);
+                }
+            }
+        }
+
+        public static void ReadThread(Socket server)
+        {
+            while (true)
+            {
+                using (NetworkStream ns = new NetworkStream(server))
+                using (StreamReader sr = new StreamReader(ns))
+                {
+                    string message = sr.ReadLine();
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    }
+}
---
+++ Servicios y Procesos/Tema 3/Networking2/Servidor/Servidor/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Servidor
+{
+    class Program
+    {
+        private static readonly object _lock = new object();
+        private static readonly List<Socket> clients = new List<Socket>();
+
+        static void Main(string[] args)
+        {
+            IPEndPoint ie = new IPEndPoint(IPAddress.Any, 31416);
+
+            using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                s.Bind(ie);
+                s.Listen(100);
+
+                while (true)
+                {
+                    Socket client = s.Accept();
+                    Console.WriteLine("Client {0} connected at {1}", (client.RemoteEndPoint as IPEndPoint).Address, (client.RemoteEndPoint as IPEndPoint).Port);
+                    lock (_lock) clients.Add(client);
+                    Thread t = new Thread(o => ClientThread(client));
+                    t.Start();
+                }
+            }
+        }
+
+        public static void ClientThread(Socket client)
+        {
+            string username = "";
+            string ip = (client.RemoteEndPoint as IPEndPoint).Address.ToString();
+
+            try
+            {
+                using (NetworkStream ns = new NetworkStream(client))
+                using (StreamReader sr = new StreamReader(ns))
+                {
+                    username = sr.ReadLine();
+                    string connectedString = String.Format("{0}@{1} has connected!", username, ip);
+                    Broadcast(connectedString, client);
+                }
+
+                while (true)
+                {
+                    using (NetworkStream ns = new NetworkStream(client))
+                    using (StreamReader sr = new StreamReader(ns))
+                    {
+                        string message = username + "@" + ip + ": " + sr.ReadLine();
+                        Console.WriteLine(message) ;
+                        Broadcast(message, client);
+                    }
+                }
+                
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("{0}@{1} disconnected!", username, ip);
+                string disconnectedString = String.Format("{0}@{1} has disconnected!", username, ip);
+                lock (_lock) clients.Remove(client);
+                Broadcast(disconnectedString, client);
+            }
+            
+        }
+
+        public static void Broadcast(string message, Socket origin)
+        {
+            foreach(Socket c in clients)
+            {
+                if(c != origin)
+                {
+                    using (NetworkStream ns = new NetworkStream(c))
+                    using (StreamWriter sw = new StreamWriter(ns))
+                    {
+                        sw.WriteLine(message);
+                    }
+                }
+            }
+        }
+    }
+}
